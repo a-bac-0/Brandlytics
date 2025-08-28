5f46ba41b27f4b830305ea21462bb9eb5ad50347
marie-adi <marie.adim@gmail.com>
2025-08-28 08:56:17 +0200

    feat:fixed model

--- models/train_yoloM.py
+++ models/train_yoloM.py
@@ -2,10 +2,10 @@ import torch
 from ultralytics import YOLO
 
 # Carga el modelo pre-entrenado
-model = YOLO("yolov5s.pt")  # Usa el modelo 's' para empezar, es más pequeño y rápido
+model = YOLO('yolov8m.pt')  # Usa el modelo 's' para empezar, es más pequeño y rápido
 
 # Inicia el entrenamiento con tus datos
-results = model.train(data='../data/marcas.yaml', epochs=100, imgsz=640)
+results = model.train(data='../data/marcas.yaml', epochs=10, imgsz=640)
 
 # El modelo entrenado se guardará en la carpeta 'runs'
 print("Entrenamiento completado. Los resultados se guardaron en la carpeta 'runs'.")
