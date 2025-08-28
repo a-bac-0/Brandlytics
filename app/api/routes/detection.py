7a9cdb0fa95505989526f623a594e87a4974ccef
Nho89 <noa1720@gmail.com>
2025-08-28 11:04:59 +0200

    fix: modified routes

--- app/api/routes/detection.py
+++ app/api/routes/detection.py
@@ -1,9 +1,37 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Form, HTTPException
+from typing import Optional
+from app.services.image_detection import ImageDetectionService
 
 router = APIRouter()
 
 #Rutas provisionales, ya que falta crear los servicios a los que llamaremos desde las rutas.
+detection_service = ImageDetectionService()
 
+@router.post("/process-image")
+async def process_image(
+    image_file: Optional[UploadFile] = File(None),
+    image_url: Optional[str] = Form(None)
+):
+    if not image_file and not image_url:
+        raise HTTPException(status_code=400, details="Debes proporcionar 'image_file' o 'image_url'.")
+    
+    try:
+        if image_file:
+            image_source = await image_file.read()
+            filename = image_file.filename
+        else:
+            image_source = image_url
+            filename = image_url.split("/")[-1]
+
+        detections = detection_service.detect_brands(image_source)
+
+        return {
+            "status": "success", 
+            "filename": filename, 
+            "detections": detections
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.post("/process-video")
 async def process_video(video: UploadFile = File(...)):
@@ -14,11 +42,3 @@ async def process_video(video: UploadFile = File(...)):
         "message": "Video recibido. El procesamiento comenzará pronto."
     }
 
-@router.post("/process-image")
-async def process_image(image: UploadFile = File(...)):
-    #igualmente aquí.
-    return {
-        "status": "received", 
-        "filename": image.filename, 
-        "message": "Imagen recibida. El procesamiento comenzará pronto."
-    }
--- app/config/model_config.py
+++ app/config/model_config.py
@@ -1,1 +1,17 @@
-##
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class Settings:
+    # Configuración del Modelo
+    MODEL_PATH: str = os.getenv("MODEL_PATH", "models/best.pt")
+
+    # Configuración de Supabase
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
+    BUCKET_CROPS: str = os.getenv("BUCKET_CROPS", "crops")
+
+# Creamos una instancia única de la configuración para usarla en toda la app
+settings = Settings()
